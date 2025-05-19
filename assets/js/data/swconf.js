561552177705b04c0b1f06d2ed9d934c53523723
Rahul Pathak <pathakraul@gmail.com>
2025-05-19 15:45:48 +0530

    Updates

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747647752',resources: [
+    cacheName: 'chirpy-1747649711',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
