40bcd58aeac86b285db4d0fb72421d2a7de0b6b3
Rahul Pathak <pathakraul@gmail.com>
2025-05-19 15:12:52 +0530

    Update the headings size

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747647369',resources: [
+    cacheName: 'chirpy-1747647752',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
