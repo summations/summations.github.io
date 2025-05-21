691b6f334c11c8d91227c6f5919bfbd1c68684d5
Rahul Pathak <pathakraul@gmail.com>
2025-05-21 15:03:01 +0530

    Update google analytics tag

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747730773',resources: [
+    cacheName: 'chirpy-1747819911',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
